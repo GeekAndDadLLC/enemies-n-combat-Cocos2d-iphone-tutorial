11141a86bbadd5db54fe94c3cba292d1710c4861
Charlie Fulton <charliefulton@gmail.com>
2013-02-06 05:34:19 -0500

    Initial Commit

---
+++ TileGame/HelloWorldLayer.h
@@ -0,0 +1,23 @@
+//
+//  HelloWorldLayer.h
+//  TileGame
+//
+//  Created by Charles Fulton on 2/6/13.
+//  Copyright charlie 2013. All rights reserved.
+//
+
+
+#import <GameKit/GameKit.h>
+
+// When you import this file, you import all the cocos2d classes
+#import "cocos2d.h"
+
+// HelloWorldLayer
+@interface HelloWorldLayer : CCLayer <GKAchievementViewControllerDelegate, GKLeaderboardViewControllerDelegate>
+{
+}
+
+// returns a CCScene that contains the HelloWorldLayer as the only child
++(CCScene *) scene;
+
+@end
---
+++ TileGame/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  TileGame
+//
+//  Created by Charles Fulton on 2/6/13.
+//  Copyright charlie 2013. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppController");
+    [pool release];
+    return retVal;
+}
